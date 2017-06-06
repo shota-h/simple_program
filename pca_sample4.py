7948b228ee9055868e3bd271a00e2199d3142444
shota harada <syo06ta18@gmail.com>
2017-06-06 23:21:11 +0900

    add function func_pca

--- pca_sample4.py
+++ pca_sample4.py
@@ -8,8 +8,6 @@ import itertools
 import sys
 
 file_ex='.PGM'
-file_ex1='.tif'
-
 class_num=10
 train_num=200
 img_pix=28
@@ -26,7 +24,7 @@ def Load_test():
         for  j in range(0,test_num):
             jj='{0:04d}'.format(j)
             img_name=str(i)+'-'+str(jj)+file_ex
-            img=cv2.imread('programe/pattern_recognition/Images/TestSamples/'+img_name,0)
+            img=cv2.imread('C:/Users/admin/programe/programe/pattern_recognition/Images/TestSamples/'+img_name,0)
             if img is None:
                 print(img_name)
                 print('not read')
@@ -44,7 +42,7 @@ def Load_train():
         for  j in range(0,train_num):
             jj='{0:04d}'.format(j)
             img_name=str(i)+'-'+str(jj)+file_ex
-            train_img=cv2.imread('programe/pattern_recognition/Images/TrainingSamples/'+img_name,0)
+            train_img=cv2.imread('C:/Users/admin/programe/programe/pattern_recognition/Images/TrainingSamples/'+img_name,0)
             if train_img is None:
                 print(img_name)
                 print('not read')
@@ -55,56 +53,63 @@ def Load_train():
             train_row[:,i*train_num+j]=train_img.ravel()
             train_class[i*train_num+j]=i
     return train_row,train_class
+def func_pca(img_row,n_com):
+    m_img=np.mean(img_row,axis=1)
+    z=img_row.T-m_img
+    cv = np.cov(z.T,bias=1)
+    w, v = np.linalg.eig(cv)
+    img_pca=[]
+    buff_img=img_row.T.dot(abs(v[:,0:n_com]))
+    img_pca=np.array(buff_img)
+    # img_pca=img_pca.transpose((1,0))
+    # print(img_pca.shape)
+    temp_pca=[]
+    for n in range(class_num):
+        mean_p=np.mean(img_pca[n:(n+1)*train_num,:],axis=0)
+        temp_pca.append(mean_p)
+    temp_pca=np.array(temp_pca)
+    # print(temp_pca.shape)
+    return img_pca
 
-def knn(train_pca,train_class):
+def knn(train_pca,train_class,n_com,flag_pca):
     score_all=np.zeros([10,1])
     test_row,test_class=Load_test()
-    print(train_pca.shape)
-    print(test_row.shape)
+    if flag_pca==1:
+        test_row=func_pca(test_row,n_com)
+    test_row=test_row.T
     # train_pca=train_row[:,:].T.dot(v)
     # print(temp_pca.shape)
     # print(temp_pca.T-train_row[:,0].T)
+    print('train_row',train_row.shape)
+    print('test_row',test_row.shape)
     for i,j in itertools.product(range(class_num),range(test_num)):
         # norm_train=np.linalg.norm(temp_pca-train_pca[i*train_num+j,:],axis=1)
-        norm_train=np.linalg.norm(train_pca.T-test_row[:,i*test_num+j].T,axis=1)
+        norm_train=np.linalg.norm(train_pca-test_row.T[:,i*test_num+j],axis=1)
+        # print(norm_train.shape)
         min_n=np.argmin(norm_train)
         # print(min_n)
         if test_class[i*test_num+j]==train_class[min_n]:
             score_all[i]+=1
-    print('score',score_all/1000)
-    print('ave',np.mean(score_all)/1000)
+    print('score',score_all/test_num)
+    print('ave',np.mean(score_all/test_num))
 
 if __name__=='__main__':
     n_com=784
+    flag_pca=0
     train_row,train_class=Load_train()
+    # print('train',train_row.shape)
     # print(img_row)
     # colors=[plt.cm.jet(float(i)/10,1) for i in range(10)]
-    # m_img=np.mean(img_row,axis=1)
-    # z=img_row.T-m_img
-    # print('z',z.shape)
-    # cv = np.cov(z.T,bias=1)
-    # print('cv',cv.shape)
-    # w, v = np.linalg.eig(cv)
-    #
-    # img_pca=[]
-    # # for k in range(10):
-    #     # for l in range(n_com):
-    # buff_img=img_row.T.dot(abs(v[:,0:n_com]))
-    # img_pca=np.array(buff_img)
-    # # img_pca=img_pca.transpose((1,0))
-    # print(img_pca.shape)
-    # temp_pca=[]
-    # for n in range(class_num):
-    #     mean_p=np.mean(img_pca[n:(n+1)*img_num,:],axis=0)
-    #     temp_pca.append(mean_p)
-    # temp_pca=np.array(temp_pca)
-    # print(temp_pca.shape)
 
     # plt.plot(temp_pca[:,0],temp_pca[:,1],'r.')
     # for n in range(class_num):
     #     plt.scatter(img_pca[n:(n+1)*img_num,0],img_pca[n:(n+1)*img_num,1],c=colors[n],s=20,alpha=0.1,marker='.')
     # plt.show()
+    if flag_pca==1:
+        train_row=func_pca(train_row,n_com)
+        train_row=train_row.T
+
 #ここより上が準備
-    knn(train_row,train_class)
+    knn(train_row.T,train_class,n_com,flag_pca)
     # knn(img_row,img_class,abs(v[:,0:n_com]))
     # knn(temp_pca,abs(v[:,0:n_com]))
